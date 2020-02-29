1dd26ae70f728af5c3dab4174f1c41c29c122249
ungsochea <sochea.ung@gmail.com>
2020-02-29 16:44:27 +0700

    Home screen app bar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: (){
+
+          },
+        ),
+        title: Text('Food Delivery'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Cart (${currentUser.cart.length})',
+              style: TextStyle(color: Colors.white,fontSize: 20.0),
+            ),
+            onPressed: (){
+
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
